430d4cfcc2eef39472de0697f667b7219ab8b5a0
2froggie <2froggie@sky.com>
2021-02-16 18:07:55 +0000

    This is in VS Code help

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("hello world")
-print("I've added some more text :)"
-# I have now entered a comment
-#print("I WROTE THIS IN VS COSE AND IN CAPITALS!"")
+print("I've added some more text :")
+
+# I am adding this from VS Code
